f0aaa3918554b21a3f88e49eb387061b6f1e3634
garytyler <mail@garytyler.com>
2018-11-20 03:12:00 +0000

    Set auth_user_id to primary key on auth2client_user table to allow user deletes/merges

--- sql/oauth2client_user.sql
+++ sql/oauth2client_user.sql
@@ -2,12 +2,15 @@
 -- https://www.mediawiki.org/wiki/Manual:SQL_patch_file
 -- extension oauth2client_user SQL schema
 --
+-- Note: null allowed on wiki_user_id to allow user deleting/merging
+--
+
 BEGIN;
 
 CREATE TABLE /*_*/`oauth2client_user` (
     `wiki_user_id` INT(10) UNIQUE,
     `auth_user_id` VARCHAR(255) UNIQUE,
-    PRIMARY KEY (`wiki_user_id`)
+    PRIMARY KEY (`auth_user_id`)
 );
 
 COMMIT;
